0525cb41d45b89f19c90b5a2a1426b96f7532e65
johannytavarez <scarlet.tavarezg@gmail.com>
2022-08-01 15:53:01 -0400

    tarea terminada

---
+++ README.md
@@ -0,0 +1,11 @@
+#Tarea 5: Servicio Web 
+
+Haga un servicio Web utilizando la librería express de nodeJS que haga las dos
+operaciones básicas de listar y almacenar contactos realizados por el servicio
+http://www.raydelto.org/agenda.php el cual usted ha utilizado en las dos tarea
+anteriores. 
+
+Johanny Tavarez 2020-10554
+
+![Peticion Post](imagenes/postman.png)
+![Peticion Get](imagenes/get.png)
---
+++ index.js
@@ -0,0 +1,51 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+app.use(bodyParser.json());
+
+const agenda = [
+    {
+        "nombre" : "Jose",
+        "apellido": "Mella",
+        "telefono": "849-251-6812",
+    },
+    {
+       "nombre" : "Dilcio",
+        "apellido": "Hernandez",
+        "telefono": "809-236-1999",
+    },
+    {
+        "nombre" : "Dionicia",
+        "apellido": "Sosa",
+        "telefono": "849-836-7365",
+   }
+]
+
+app.post("/agregarContacto", (request, response) =>{
+    const contacto = {
+        nombre: request.body.nombre,
+        apellido : request.body.apellido,
+        telefono: request.body.telefono
+    }
+    agenda.push(contacto);
+
+    const respuesta = {
+        data: contacto,
+        menssage: "se agrego correctamente", 
+        exito: true
+    }
+    response.send(respuesta)
+});
+
+app.get('/Listar', (request, response) => {
+                
+    const respuesta = {
+        data: agenda
+    }
+    response.send(respuesta)
+});
+
+app.listen(8080, () => {
+    console.log("El servidor está escuchando en el puerto 8080")
+})
+
